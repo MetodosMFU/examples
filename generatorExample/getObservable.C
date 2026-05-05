eeadc9504d39697fadef8d43d7262afc8fe33888
juanan <juanan318@gmail.com>
2026-05-05 12:59:04 +0200

    Update getObservable

--- generatorExample/getObservable.C
+++ generatorExample/getObservable.C
@@ -26,10 +26,18 @@ ROOT::RDataFrame df("AnalysisTree", fileName);
 auto df_with_val = df.Define("amp_ch0", [](const std::map<int, double>& m) {
     if (m.count(0)) return m.at(0);
     return 0.0;
-}, {"totalAmplitude"});
+}, {obsString});
+
+df_with_val = df_with_val.Define("amp_ch1", [](const std::map<int, double>& m) {
+    if (m.count(1)) return m.at(1);
+    return 0.0;
+}, {obsString});
 
 auto h = df_with_val.Histo1D("amp_ch0");
 h->DrawClone();
+
+auto h1 = df_with_val.Histo1D("amp_ch1");
+h1->DrawClone("SAME");
 
 }
 
